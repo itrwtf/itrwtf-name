e8cd78967cd7a6beac0402f61e8c9e02b4ec398b
itr <131329791+tristanreal@users.noreply.github.com>
2025-07-11 15:06:04 -0400

    Add files via upload

---
+++ obfuscated (1).lua
@@ -0,0 +1,349 @@
+-- This script was generated using the MoonVeil Obfuscator v1.3.0 [https://moonveil.cc]
+
+local u,la,z,q,ha,i,h,l,g,fa
+la,ha={[-23611]=15412,[-13633]=2832,[-17690]=-10618,[19117]=-24655,[30058]=3832,[-19727]=-8611,[-16235]=-8611,[-25665]=16282,[3233]=16282,[23127]=16282,[23516]=-10260,[-13515]=11124,[-17339]=-10260,[25626]=16282,[631]=-10260,[10119]=-4638,[-663]=15542,[-7610]=15542,[24860]=3832,[-3127]=-22087},function(O)
+    return la[O- -27505]
+end
+g=ha(-44844)
+repeat
+    while true do
+        if g==1.1131792064350078e-05*-773550202 then
+            u=((function()
+                local function qa(t,W,na)
+                    if W>na then
+                        return
+                    end
+                    return t[W],qa(t,W+1,na)
+                end
+                return qa
+            end)());
+            g=ha(-8388);
+            break;
+        elseif g==-10241272134720/998174672 then
+            z=(getfenv());
+            g=ha(-24272);
+        elseif g==-2301507559596/-141352878 then
+            q,i,h=(string.char),(string.byte),(bit32 .bxor);
+            g=ha(-35115);
+        elseif g==-251894851- -251898683 then
+            l=(function(...)
+                return{[1]={...},[2]=fa('#',...)}
+            end);
+            g=ha(-43740);
+        elseif g==-14761125811804/-949757162 then
+            fa=(select);
+            g=ha(-2645);
+        end
+    end
+until g==1480781302-1480805957
+local e,s,F,X,A,ja,Y,R,ta
+s,ja={[-10485]=16168,[-20908]=-13276,[-24632]=-7329,[27949]=-12040,[10883]=-26709,[-3667]=-13276,[-30987]=10574,[29852]=22686,[30191]=31143,[-25862]=13237,[-10455]=-32333,[-17859]=19338,[4558]=31143,[-12122]=10574,[9447]=5213,[9773]=14007,[26636]=-25770,[-22198]=22686,[18548]=-22916,[-25247]=-4092,[27540]=12399,[-8766]=5213},function(T)
+    return s[T- -29824]
+end
+Y=ja(-20377)
+repeat
+    while true do
+        if Y==-1.107149497224138e-05*1679357670 then
+            ta=ta[u(R[1],1,R[2])]
+            Y=ja(-52022)
+        elseif Y==8897554155735/-1214020215 then
+            R='x)8Q +['
+            Y=ja(-55071)
+        elseif Y==1.2516105506750761e-05*1812544644 then
+            F,X=e,ta;
+            Y=ja(-60811);
+        elseif Y==1439213407+-1439208194 then
+            e='+L\229lO\227$'
+            Y=ja(-1875)
+        elseif Y==5.6038691994489835e-05*-466695404 then
+            A='\176\219I\231\206'
+            Y=ja(-18941)
+        elseif Y==-1546579042- -1546574432 then
+            R='\165Z'
+            Y=ja(-40279)
+        elseif Y==2.964795815707877e-05*1050426469 then
+            X[(function(ma,B)
+                local xa,Aa,o,wa,M,va,P,_
+                P,va={[27153]=7079,[-3268]=-5372,[-28569]=-11173,[-22580]=-30146,[-16399]=5622},function(Ha)
+                    return P[Ha+8915]
+                end
+                xa=va(-25314)
+                repeat
+                    while true do
+                        if xa==7079 then
+                            return o
+                        elseif xa==-30146 then
+                            if(M>=0 and _>Aa)or((M<0 or M~=M)and _<Aa)then
+                                xa=7079
+                            else
+                                xa=32081
+                            end
+                        elseif xa==32081 then
+                            o=o..q(h(i(ma,(wa-248)+1),i(B,(wa-248)%#B+1)))
+                            xa=va(-37484)
+                        elseif xa==5622 then
+                            o='';
+                            M,Aa,_=1,(#ma-1)+248,248
+                            xa=va(-12183)
+                        elseif xa==-11173 then
+                            _=_+M;
+                            wa=_;
+                            if _~=_ then
+                                xa=va(18238)
+                            else
+                                xa=-30146
+                            end
+                        elseif xa==-5372 then
+                            wa=_;
+                            if Aa~=Aa then
+                                xa=7079
+                            else
+                                xa=va(-31495)
+                            end
+                        end
+                    end
+                until xa==21118
+            end)('Q\4r\0','\31e')]=F;
+            Y=ja(-20051);
+            break;
+        elseif Y==-68736889- -68750126 then
+            e=(function(ua,Ka)
+                local oa,f,C,y,ea,Ea,Z,r
+                Z,oa={[-11540]=-4727,[31255]=-24455,[-22389]=-7320,[10846]=-11012},function(k)
+                    return Z[k+-17343]
+                end
+                C=oa(28189)
+                repeat
+                    while true do
+                        if C==-11012 then
+                            y='';
+                            f,r,ea=1,152,(#ua-1)+152
+                            C=20462
+                        elseif C==20462 then
+                            Ea=r;
+                            if ea~=ea then
+                                C=oa(48598)
+                            else
+                                C=16802
+                            end
+                        elseif C==-24455 then
+                            return y
+                        elseif C==16802 then
+                            if(f>=0 and r>ea)or((f<0 or f~=f)and r<ea)then
+                                C=-24455
+                            else
+                                C=oa(5803)
+                            end
+                        elseif C==-7320 then
+                            r=r+f;
+                            Ea=r;
+                            if r~=r then
+                                C=-24455
+                            else
+                                C=16802
+                            end
+                        elseif C==-4727 then
+                            y=y..q(h(i(ua,(Ea-152)+1),i(Ka,(Ea-152)%#Ka+1)))
+                            C=oa(-5046)
+                        end
+                    end
+                until C==30057
+            end)(e,ta)
+            Y=ja(-33491)
+        elseif Y==-68124461935719/2106963843 then
+            ta=l((function(V,L)
+                local ga,D,U,ca,ra,N,Ca,G
+                D,U={[-25340]=3590,[11160]=23784,[17303]=-21473,[24786]=9483,[-11794]=-21473,[-29771]=22149},function(Da)
+                    return D[Da-27183]
+                end
+                N=U(38343)
+                repeat
+                    while true do
+                        if N==23784 then
+                            ga='';
+                            ca,ra,Ca=(#V-1)+147,1,147
+                            N=-27901
+                        elseif N==3590 then
+                            ga=ga..q(h(i(V,(G-147)+1),i(L,(G-147)%#L+1)))
+                            N=U(51969)
+                        elseif N==-21473 then
+                            if(ra>=0 and Ca>ca)or((ra<0 or ra~=ra)and Ca<ca)then
+                                N=22149
+                            else
+                                N=U(1843)
+                            end
+                        elseif N==22149 then
+                            return ga
+                        elseif N==9483 then
+                            Ca=Ca+ra;
+                            G=Ca;
+                            if Ca~=Ca then
+                                N=22149
+                            else
+                                N=U(15389)
+                            end
+                        elseif N==-27901 then
+                            G=Ca;
+                            if ca~=ca then
+                                N=U(-2588)
+                            else
+                                N=U(44486)
+                            end
+                        end
+                    end
+                until N==-23713
+            end)(ta,R))
+            Y=1.8621236384290184e-05*1377083641
+        elseif Y==-6439417078990/-251117930 then
+            ta=z[u(ta[1],1,ta[2])]
+            Y=ja(-54456)
+        elseif Y==-10591980008452/-1001700398 then
+            X[(function(H,Ga)
+                local m,_a,E,j,da,S,Ba,ba
+                da,S={[-16773]=18662,[-18458]=-8509,[27220]=-28454,[9929]=-5769},function(za)
+                    return da[za- -19223]
+                end
+                ba=S(-37681)
+                repeat
+                    while true do
+                        if ba==18662 then
+                            return j
+                        elseif ba==-25255 then
+                            j=j..q(h(i(H,(E-141)+1),i(Ga,(E-141)%#Ga+1)))
+                            ba=S(7997)
+                        elseif ba==-8509 then
+                            j='';
+                            m,Ba,_a=1,141,(#H-1)+141
+                            ba=31551
+                        elseif ba==-5769 then
+                            if(m>=0 and Ba>_a)or((m<0 or m~=m)and Ba<_a)then
+                                ba=18662
+                            else
+                                ba=-25255
+                            end
+                        elseif ba==31551 then
+                            E=Ba;
+                            if _a~=_a then
+                                ba=18662
+                            else
+                                ba=S(-9294)
+                            end
+                        elseif ba==-28454 then
+                            Ba=Ba+m;
+                            E=Ba;
+                            if Ba~=Ba then
+                                ba=S(-35996)
+                            else
+                                ba=-5769
+                            end
+                        end
+                    end
+                until ba==-18387
+            end)('p\25\185~GU\t\132oFQ','4p\202\14+')]=F;
+            Y=ja(367);
+        elseif Y==-23758656421360/1973310334 then
+            ta='B8\151'
+            Y=ja(-55686)
+        elseif Y==375062884-375066976 then
+            A='(EY'
+            Y=ja(-3188)
+        elseif Y==-1416833548- -1416807778 then
+            R=l((function(ia,n)
+                local sa,Fa,d,Q,K,Ja,x,ka
+                Ja,ka={[21483]=-24510,[-23470]=-18820,[25523]=-27671},function(p)
+                    return Ja[p- -1985]
+                end
+                sa=ka(19498)
+                repeat
+                    while true do
+                        if sa==-24510 then
+                            K='';
+                            x,Fa,Q=(#ia-1)+234,1,234
+                            sa=-15102
+                        elseif sa==-27671 then
+                            Q=Q+Fa;
+                            d=Q;
+                            if Q~=Q then
+                                sa=ka(-25455)
+                            else
+                                sa=-13396
+                            end
+                        elseif sa==-18820 then
+                            return K
+                        elseif sa==-13396 then
+                            if(Fa>=0 and Q>x)or((Fa<0 or Fa~=Fa)and Q<x)then
+                                sa=-18820
+                            else
+                                sa=-164
+                            end
+                        elseif sa==-15102 then
+                            d=Q;
+                            if x~=x then
+                                sa=-18820
+                            else
+                                sa=-13396
+                            end
+                        elseif sa==-164 then
+                            K=K..q(h(i(ia,(d-234)+1),i(n,(d-234)%#n+1)))
+                            sa=ka(23538)
+                        end
+                    end
+                until sa==-12282
+            end)(R,A))
+            Y=ja(-11276)
+        elseif Y==-865787241+865773965 then
+            ta='\194;\200?'
+            Y=-1629301242+1629296632
+        elseif Y==-20100395135888/877133668 then
+            ta=ta[u(R[1],1,R[2])]
+            Y=ja(-40309)
+        elseif Y==6446706993060/-241368340 then
+            R=l((function(w,pa)
+                local I,ya,a,Ia,c,J,v,aa
+                a,v={[9589]=-24421,[-1967]=23243,[22717]=-2114,[26518]=18531,[-915]=16068,[30942]=11504},function(b)
+                    return a[b- -14233]
+                end
+                aa=v(-4644)
+                repeat
+                    while true do
+                        if aa==-2114 then
+                            return I
+                        elseif aa==23243 then
+                            I=I..q(h(i(w,(Ia-102)+1),i(pa,(Ia-102)%#pa+1)))
+                            aa=v(-15148)
+                        elseif aa==16068 then
+                            c=c+ya;
+                            Ia=c;
+                            if c~=c then
+                                aa=-2114
+                            else
+                                aa=v(12285)
+                            end
+                        elseif aa==-24421 then
+                            I='';
+                            ya,c,J=1,102,(#w-1)+102
+                            aa=v(16709)
+                        elseif aa==11504 then
+                            Ia=c;
+                            if J~=J then
+                                aa=-2114
+                            else
+                                aa=18531
+                            end
+                        elseif aa==18531 then
+                            if(ya>=0 and c>J)or((ya<0 or ya~=ya)and c<J)then
+                                aa=v(8484)
+                            else
+                                aa=v(-16200)
+                            end
+                        end
+                    end
+                until aa==20082
+            end)(R,A))
+            Y=33409938984255/-1796909535
+        elseif Y==-9.887652732250285e-06*-1635170696 then
+            R='\252\180*\134\162\224\183(\158\171\194'
+            Y=627066461+-627092614
+        end
+    end
+until Y==-1990038447- -1990052454
